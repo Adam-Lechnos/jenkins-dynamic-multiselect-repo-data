c1137bfca4d0e4902a91be057c157771c6fad378
Adam M. Lechnos <adam.lechnos@gmail.com>
2022-02-28 14:59:49 -0500

    Update multi_selector_json_dyndir.sh

--- multi_selector_json_dyndir.sh
+++ multi_selector_json_dyndir.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-authToken='add token here'
+source 'auth.cfg with token(s)'
+
 repoName=$1
 repoRelease=$2
 region=$3
 env=$4
 repoNameTrim=`echo $repoName | cut -d':' -f2 | cut -d'/' -f2 | sed 's/.git//g'`
 repoOwner=`echo $repoName | cut -d':' -f2 | cut -d'/' -f1`
+logDir='log directory here'
 
 payload_root='query RepoFiles{
 repository(owner: \"'$repoOwner'\", name: \"'$repoNameTrim'\") {
@@ -37,30 +39,30 @@ repository(owner: \"'$repoOwner'\", name: \"'$repoNameTrim'\") {
 payloadFrmt="$(echo $payload)"
 
 logretention=20
-loglist=$(ls -l /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/ | grep .log | awk '{print $9}' | wc -l)
+loglist=$(ls -l $logDir/multiselect_json_create/logs/ | grep .log | awk '{print $9}' | wc -l)
 
-if [ ! -d /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs ]; then
-  mkdir /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs
+if [ ! -d $logDir/multiselect_json_create/logs ]; then
+  mkdir $logDir/multiselect_json_create/logs
 fi
 
 if [ $loglist -gt $logretention ]
 then
 
  delcount=`expr $loglist - $logretention`
- find /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/ -type f -printf '%T+ %p\n' | sort | head -n $delcount | awk '{print $2}' | sed 's/[^\]*logs[^\]//' | xargs -I {} rm /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/{}
+ find $logDir/multiselect_json_create/logs/ -type f -printf '%T+ %p\n' | sort | head -n $delcount | awk '{print $2}' | sed 's/[^\]*logs[^\]//' | xargs -I {} rm $logDir/multiselect_json_create/logs/{}
 
 fi
 
 ##curl root create/aws dir and check for jsons
 display_root=`curl \
 -s -H "Authorization: bearer $authToken" \
--X POST -d "{ \"query\": \"$payloadFrmt_root\"}" https://api.github.factset.com/graphql \
+-X POST -d "{ \"query\": \"$payloadFrmt_root\"}" https://api.github.com/graphql \
 | jq -r -e '.data.repository.object.entries[].name' 2> /dev/null | grep .json`
 
 ##curl via dynamic dir variables
 display_dyn=`curl \
 -s -H "Authorization: bearer $authToken" \
--X POST -d "{ \"query\": \"$payloadFrmt\"}" https://api.github.factset.com/graphql \
+-X POST -d "{ \"query\": \"$payloadFrmt\"}" https://api.github.com/graphql \
 | jq -r -e '.data.repository.object.entries[].name' 2> /dev/null | grep .json`
 
 if [ ! -z "$display_root" ]; then echo "$display_root"; else echo "$display_dyn"; fi
@@ -74,7 +76,7 @@ then
 
  curl -s -I \
  -H "Authorization: bearer $authToken" \
- https://api.github.factset.com/graphql > /var/lib/jenkins/git/cloud-market-data/jenkins_tools/multiselect_json_create/logs/cURLheader_$timestamp.log
+ https://api.github.factset.com/graphql > /'add directory here'/multiselect_json_create/logs/cURLheader_$timestamp.log
 
 else
 
